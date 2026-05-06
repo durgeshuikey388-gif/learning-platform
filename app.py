057bd030facd5b99aa06b4c514810e675c979208
Durgesh <durgeshuikey388@gmail.com>
2026-05-06 22:48:10 +0530

    final render fix

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def download():
     return send_file("static/certificate.pdf", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
